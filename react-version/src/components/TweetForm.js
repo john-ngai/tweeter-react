7c0f888a1f3159dcb27c7838cbc4ca68a0439eaa
mrjohnming <john.ngai@hotmail.com>
2022-02-20 14:31:40 -0500

    Replace hard-coded values with variables

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,10 +1,12 @@
 import React from 'react'; // Optional for React v17 & up.
 
 function TweetForm() {
+  const placeholderText = 'What are you humming about?';
+  
   return (
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <textarea className="form__textarea" name="text" placeholder={placeholderText}></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
       </form>

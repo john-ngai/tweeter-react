b7d7a171a8b2d0b3962d449767fe43d25d50e652
mrjohnming <john.ngai@hotmail.com>
2022-02-20 15:45:59 -0500

    Add .preventDefault to the form

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -5,7 +5,7 @@ function TweetForm() {
   
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" action="/tweets" className="newtweet__form" onClick={(event) => event.preventDefault()}>
         <textarea className="form__textarea" name="text" placeholder={placeholderText}></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>

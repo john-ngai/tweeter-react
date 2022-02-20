4000fd11c4c4864bf944e7cea666cb974a06c6ff
mrjohnming <john.ngai@hotmail.com>
2022-02-20 13:52:39 -0500

    Import all the components

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react'; // Optional for React v17 & up.
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+import TweetList from './components/TweetList';
+import TweetForm from './components/TweetForm';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      <Profile />
+      <TweetList />
+      <TweetForm />
     </div>
   );
 }
